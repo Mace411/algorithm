1f7193523aaebb9c87f73cbf213d426c4cb39b87
xieyiquan <850509131@qq.com>
2021-02-19 18:34:24 +0800

    合成算法题原来的做法错误，这里修正

--- src/lc/Compose.java
+++ src/lc/Compose.java
@@ -1,31 +1,51 @@
 package lc;
 
 public class Compose {
+
     /**
-     * f(i) = Math.ceil(f(i - 1) * 2.5)
-     * f(2) = 3
+     * 原来做的方法是错误的，只是巧合前面几个数对了
+     * 这里的方法是从大往小推，一阶一阶推到1阶，就像是一颗树，18阶其实有点像是高为18的树的节点数有多少个，所以有点接近指数递增
      * @param level
+     * @param composeNum
      * @return
      */
-    public static int getNum1(int level) {
-        if (level <= 1) {
-            return 1;
+    public static int getNum1(int level, int composeNum) {
+        if (level == 1) {
+            return composeNum;
         }
         int num = 0;
-        for (int i = 1; i < level; i++) {
-            num = (int) Math.ceil(i * 2.5);
+        //多少个5的
+        int five = composeNum / 2;
+        //多少个3的，这里其实就只会有一个3的
+        int three = composeNum % 2;
+        //从小一阶开始推算需要多少个
+        for (int i = level - 1; i > 0; i--) {
+            num = five * 5 + three * 3;
+            five = num / 2;
+            three = num % 2;
         }
         return num;
     }
 
-    public static int getNUm2(int level) {
+    public static int getNum(int level, int composeNum) {
         if (level == 1) {
-            return 1;
+            return composeNum;
         }
-        return (int) Math.ceil(getNUm2(level - 1) * 2.5);
+        int num = 0;
+        //多少个5的
+        int five = composeNum / 2;
+        //多少个3的，这里其实就只会有一个3的
+        int three = composeNum % 2;
+        //从小一阶开始推算需要多少个
+        num = five * 5 + three * 3;
+        return getNum(level - 1, num);
+    }
+
+    public static int getNum2(int level) {
+        return getNum(level - 1, 3);
     }
 
     public static void main(String[] args) {
-        System.err.println(getNUm2(3));
+
     }
 }
